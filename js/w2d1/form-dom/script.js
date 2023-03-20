6a60b09a517f3eef9decdae02ce84f142c99f3d2
narcisolobo <narcisolobo@gmail.com>
2023-03-20 14:00:39 -0700

    w2d1 afternoon code

---
+++ js/w2d1/form-dom/script.js
@@ -0,0 +1,10 @@
+var searchInput = document.querySelector('#search');
+
+function searchAlert() {
+  alert(searchInput.value);
+}
+
+function changeImage(image) {
+  image.src = './images/tanooki.webp'
+}
+
